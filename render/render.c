206b5a085f3bfefb5355be629629839c88668629
Fiberteror <96434314+Fiberteror@users.noreply.github.com>
2022-01-10 19:27:55 -0300

    WarZone Update

--- render/render.c
+++ render/render.c
@@ -1,15 +1,24 @@
 #include"render.h"
-#include "raylib.h"
+#include"raylib.h"
+#include"forms.h"
 
 renderer_comp renderer;
 
 void render_init(void){
 
     for (int i = 0; i < MAX_COLUMNS; i++){
-        renderer.heights[i] = (float)GetRandomValue(1, 12);
+        renderer.heights[i] = (float)GetRandomValue(1, HEIGHT);
         renderer.positions[i] = (Vector3){ (float)GetRandomValue(-150, 150),renderer.heights[i]/2.0f, (float)GetRandomValue(-15, 15) };
         renderer.colors[i] = (Color){ GetRandomValue(20, 255), GetRandomValue(10, 55), 30, 255 };
     }
+}
+
+void render_campo(void){
+    DrawCube((Vector3){ 165.0f/2, 0.0f, -100.0f }, 165.0f, 10.0f, 1.0f, GRAY);
+    DrawCube((Vector3){ 0.0f, 0.0f, -15.0f }, 1.0f, 10.0f, 200.0f, GRAY);
+    DrawCube((Vector3){ 145.0f, 0.0f, -5.0f }, 1.0f, 10.0f, 165.0f, GRAY);
+    DrawCube((Vector3){ 145.0f, 0.0f, -5.0f }, 1.0f, 10.0f, 200.0f, GRAY);
+    DrawCube((Vector3){ 165.0f/2-15, 0.0f, 165.0f/2 }, 160.0f, 10.0f, 0.0f, GRAY);
 }
 
 void render(void){
@@ -18,7 +27,8 @@ void render(void){
                 
     for (int i = 0; i < 30; i++)
     {
-        DrawCube(renderer.positions[i], 2.0f,renderer.heights[i], 2.0f,renderer.colors[i]);
-        DrawCubeWires(renderer.positions[i], 2.0f,renderer.heights[i], 2.0f, MAROON);
+        DrawCylinder((Vector3) {1.0f,-4.0f,-82.5f/4+15},0.2f,0.3f,12.0f,15, BLACK);
+        DrawCylinder((Vector3) {130.0f,0.0f,-82.5f/4+15},0.2f,0.3f,12.0f,15, BLACK);
+        render_campo();
     }
 }
