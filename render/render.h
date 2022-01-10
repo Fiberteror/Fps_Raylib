ced7e8ed097a40b9b01dbd460a99e92046bf6a24
Fiberteror <96434314+Fiberteror@users.noreply.github.com>
2022-01-10 00:30:57 -0300

    Update render.h

--- render/render.h
+++ render/render.h
@@ -1,3 +1,11 @@
 #define CUBES 15
+#define MAX_COLUMNS 20
+#include "raylib.h"
+
+typedef struct{
+    float heights[MAX_COLUMNS] = { 0 };
+    Vector3 positions[MAX_COLUMNS] = { 0 };
+    Color colors[MAX_COLUMNS] = { 0 };
+}render_init;
 
 void render(void);
