8a573e34d28c95b4673b15b39413eb8eaaca6805
Fiberteror <96434314+Fiberteror@users.noreply.github.com>
2022-01-10 01:08:32 -0300

    Update render.h

--- render/render.h
+++ render/render.h
@@ -3,9 +3,9 @@
 #include "raylib.h"
 
 typedef struct{
-    float heights[MAX_COLUMNS] = { 0 };
-    Vector3 positions[MAX_COLUMNS] = { 0 };
-    Color colors[MAX_COLUMNS] = { 0 };
+    float heights[MAX_COLUMNS];
+    Vector3 positions[MAX_COLUMNS];
+    Color colors[MAX_COLUMNS];
 }render_init;
 
 void render(void);
