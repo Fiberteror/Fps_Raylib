ca2612d284d72906158c51591792995ea666f0c7
Fiberteror <96434314+Fiberteror@users.noreply.github.com>
2022-01-10 00:46:13 -0300

    Camera declarations on typedef struct;

--- render/camera_properties.h
+++ render/camera_properties.h
@@ -1,1 +1,12 @@
+#include "raylib.h"
 
+typedef struct
+{
+    Camera camera;
+    camera.position;
+    camera.target;
+    camera.fovy;
+    camera.projection;
+}camera_properties;
+
+void setCamera(void);
